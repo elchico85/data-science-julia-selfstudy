4dd047bf40f764f3005f04b01f9ac4ecba09a892
elchico85 <valerio.parra85@gmail.com>
2025-05-15 09:38:00 +0200

    Implement Sicherman puzzle logic to populate two_standard dictionary

--- Sicherman puzzle.jl
+++ Sicherman puzzle.jl
@@ -1,1 +1,14 @@
-two_standard=Dict
+two_standard = Dict{Int, Int}()
+
+for i in [1, 2, 3, 4, 5, 6]
+    for j in [1, 2, 3, 4, 5, 6]
+        s = i+j
+        if haskey(two_standard, s)
+            two_standard[s] += 1
+        else
+            two_standard[s] = 1
+        end
+    end
+end
+
+two_standard
